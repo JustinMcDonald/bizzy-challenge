c436c289e0c9d944499446316cd0ac5b0e8ee664
Justin McDonald <justin.m.mcdonald@gmail.com>
2016-04-02 17:24:20 -0400

    first attempt at php coupon create serv

--- api/coupon.php
+++ api/coupon.php
@@ -9,19 +9,44 @@ $response = array('error' => 0, 'message' => "Success", 'data' => "EXAMPLEcode16
 $name = (isset($_POST['name'])) ? $_POST['name'] : "";
 if (isset($_POST['type'])) $type = $_POST['type'];
 else {
-	echo json_encode(array('error' => 1, 'message' => "Please provide a type", 'data' => ""));
+	echo json_encode(array('error' => 1, 'message' => "Please provide a discount type (percentage/fixed).", 'data' => ""));
 	exit;
 }
 if (isset($_POST['amount'])) $amount = $_POST['amount'];
 else {
-	echo json_encode(array('error' => 1, 'message' => "Please provide an amount", 'data' => ""));
+	echo json_encode(array('error' => 1, 'message' => "Please provide a discount amount.", 'data' => ""));
 	exit;
 }
 if (isset($_POST['min'])) $min = $_POST['min'];
 else {
-	echo json_encode(array('error' => 1, 'message' => "Please provide a minimum", 'data' => ""));
+	echo json_encode(array('error' => 1, 'message' => "Please provide a minimum order amount.", 'data' => ""));
 	exit;
 }
+
+$discount = array("discount_type"=>$type, "value"=>$amount, "minimum_order_amount"=>$min);
+
+if ($name != "") $discount["code"] = $name;
+
+$request = array("discount"=>$discount);
+
+$requestJSON = json_encode($request);
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => $requestType,
+        'content' => $requestJSON
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url . $endpoint, false, $context);
+if ($result === FALSE) { 
+	echo json_encode(array('error' => 1, 'message' => "Error: unable to create shopify coupon.", 'data' => ""));
+	exit;
+}
+
+$response['data'] = $result;
 
 echo json_encode($response);
 ?>
